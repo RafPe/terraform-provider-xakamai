a7122799c60d12c5ecce6645a5e524b6ebc56674
RafPe <rafal@pieniazek.nl>
2018-03-13 16:47:28 +0100

    tf changes

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ provider "xakamai" {
 }
 
 resource "xakamai_network_list" "my-server" {
-  name  = "MY_TF_TEST_1"
-  type  = "IP"
-  items = ["1.2.3.3/32", "9.8.7.6/24"]
+  name        = "MY_TF_TEST_1"
+  description = "changed-in-config"
+  type        = "IP"
+  items       = ["1.2.3.3/32", "9.8.7.6/24"]
 }
